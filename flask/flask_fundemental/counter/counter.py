a103819c1665d9d823f40a3a3868a173988e9cd5
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-09-20 22:55:51 +0300

    submit

---
+++ flask/flask_fundemental/counter/counter.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route("/")
+def count():
+    if 'count' not in session:
+        session['count'] = 0
+    session['count'] += 1
+    return render_template('index.html')
+
+@app.route('/destroy')
+def destroy():
+    session.clear()	
+    return redirect('/')
+
+@app.route('/add_two')
+def add_two():
+    session['count'] += 1	
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
