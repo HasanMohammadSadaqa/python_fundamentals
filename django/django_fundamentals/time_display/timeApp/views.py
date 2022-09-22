7b7d453c9c5e737f89d847edd090bfaea28d0640
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-09-22 11:50:53 +0300

    submit

---
+++ django/django_fundamentals/time_display/timeApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from time import gmtime, strftime
+    
+def index(request):
+    context = {
+        "date": strftime("%Y-%m-%d", gmtime()),
+        "time": strftime("%H:%M %p", gmtime())
+    }
+    return render(request,'time.html', context)
+
+
