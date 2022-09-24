2133ca1f96204f29dd922f792531d0cd5bb2660b
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-09-24 15:29:12 +0300

    edit the error

--- django/django_fundamentals/Dojo_Servey/form/views.py
+++ django/django_fundamentals/Dojo_Servey/form/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "index.html")
 
 def creat_user(request):
     if request.method == "POST": 
-        firstname_from_form = request.form["first_name"]
-        lastname_from_form = request.form["last_name"]
-        department_from_form = request.form["department"]
-        username_from_form = request.form["user_name"]
-        password_from_form = request.form['user_password']
-        email_from_form = request.form["email"]
-        number_from_form = request.form["contact_no"]
-    return render(request, "show.html", first_name =firstname_from_form, last_name = lastname_from_form, department = department_from_form,
-            username =username_from_form, password = password_from_form, email = email_from_form, num = number_from_form)
+        firstname_from_form = request.POST["first_name"]
+        lastname_from_form = request.POST["last_name"]
+        department_from_form = request.POST["department"]
+        username_from_form = request.POST["user_name"]
+        password_from_form = request.POST['user_password']
+        email_from_form = request.POST["email"]
+        number_from_form = request.POST["contact_no"]
+        context = {
+            "first_name" : firstname_from_form,
+            "last_name" : lastname_from_form,
+            "department" : department_from_form,
+            "username" : username_from_form,
+            "password" :  password_from_form, 
+            "email" : email_from_form,
+            "num" : number_from_form
+        }
+    return render(request, "show.html", context)
+    
 
