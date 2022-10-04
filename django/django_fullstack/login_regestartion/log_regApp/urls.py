89ba9a8c9ac46c5345a17aed119e25c59d7ea86f
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-10-04 11:08:39 +0300

    submit

--- django/django_fullstack/login_regestartion/log_regApp/urls.py
+++ django/django_fullstack/login_regestartion/log_regApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.root),
-    path('reg/', views.registration),
-    path("login/", views.check_login),
-    path("success/<int:user_id>", views.show_registors),
+    path('reg', views.registration),
+    path("login", views.check_login),
+    path("success/", views.show_registors),
     path("logout/", views.logout_user),
 ]
--- django/django_fullstack/login_regestartion/log_regApp/views.py
+++ django/django_fullstack/login_regestartion/log_regApp/views.py
@@ -30,23 +30,41 @@ def registration(request):
 
 
 def check_login(request):
-    user = User.objects.filter(email=request.POST["email"])
-    if user:  # if True (email is found with registration database)
-        logged_user = user[0]
-        if bcrypt.checkpw(
-            request.POST["password"].encode(), logged_user.password.encode()
-        ):
-            request.session["userid"] = logged_user.id
-            return redirect(f"/success/{logged_user.id}")
+    user = User.objects.filter(email=request.POST['email'])
+    if user :
+        loggd_user = user[0]
+        if bcrypt.checkpw(request.POST['pass'].encode(), loggd_user.Password.encode()):
+            print('1')
+            request.session['user_id'] = loggd_user.id
+            request.session['name'] = loggd_user.first_name
+            return redirect("/success")
         return redirect("/")
 
-def show_registors(request,user_id):
-    context={
-        "user" : User.objects.get(id=user_id)
-        }
-    return render(request, 'welcoming.html', context)
+
+
+
+
+    # user = User.objects.filter(email=request.POST["email"])
+    # print("1")
+    # if user:  # if True (email is found with registration database)
+    #     print("2")
+    #     logged_user = user[0]
+    #     print("3")
+    #     # if bcrypt.checkpw(request.POST['pass'].encode(), logged_user.Password.encode()):
+    #     if bcrypt.checkpw(request.POST['pass'].encode(), logged_user.Password.encode()):
+            
+    #             print("4")
+    #             request.session["userid"] = logged_user.id
+    #             request.session['name'] = logged_user.first_name
+    #             print("5")
+    #             return redirect("/success")
+    #     return redirect("/")
+
+def show_registors(request):
+    return render(request, 'welcoming.html')
 
 def logout_user(request):
     del request.session["userid"]
+    del request.session["name"]
     return redirect("/")
 
